dcc2e53962cba26abf90913d62fc714be385000b
dblee <danbi.db@gmail.com>
2021-12-23 14:00:26 +0900

    feat: Apply page routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
+import loadable from '@loadable/component';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import { SWRConfig } from 'swr';
 import Header from '@layouts/Header';
+
+const AlarmDashboard = loadable(() => import('@pages/AlarmDashboard'));
+const CriticalWarningStatus = loadable(() => import('@pages/CriticalWarningStatus'));
 
 const App = () => {
   return (
@@ -10,6 +15,11 @@ const App = () => {
       }}
     >
       <Header />
+      <Switch>
+        <Route exact path="/" component={AlarmDashboard} />
+        <Route path="/status" component={CriticalWarningStatus} />
+        <Redirect exact path="/" to="/" />
+      </Switch>
     </SWRConfig>
   );
 };
