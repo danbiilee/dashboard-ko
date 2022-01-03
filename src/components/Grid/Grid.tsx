6839d6bd45f2260c917fa7cb3356695f7db50def
dblee <danbi.db@gmail.com>
2022-01-03 15:31:18 +0900

    refactor: Seperate grid class, column data

--- src/components/Grid/Grid.tsx
+++ src/components/Grid/Grid.tsx
@@ -4,39 +4,11 @@ import { GridData } from '@customTypes/common';
 import { useGrid } from '@hooks/useGrid';
 import { useEmsUrl } from '@hooks/useEmsUrl';
 import Indicator from '@components/Indicator';
+import { classNames, columns } from './data';
 
 interface GridProps {
   type: string;
 }
-
-interface IndexClassNames {
-  [index: string]: string;
-}
-
-interface IndexColumns {
-  [index: string]: string[];
-}
-
-const classNames: IndexClassNames = {
-  NO: 'no',
-  SERVICE_NAME: 'service-nm',
-  TEAM_NAME: 'team-nm',
-  CNT: 'cnt',
-  ALARMSEVERITY: 'grade',
-  ALARM_NAME: 'alarm-nm',
-  DAY: 'day',
-  THIS_WEEK: 'this-week',
-  LAST_WEEK: 'last-week',
-  INC: 'ratio',
-  CONDITIONLOGTEXT: 'log',
-};
-
-const columns: IndexColumns = {
-  month: ['No', '서비스명', '팀명', '건수'],
-  notTaken: ['No', '팀명', '등급', '알람명', '기간', '비고'],
-  week: ['No', '알람명', '서비스명', '건수'],
-  prepare: ['No', '알람명', '서비스명', '금주', '전주', '증감율'],
-};
 
 const Grid: React.FC<GridProps> = ({ type }) => {
   let { data, isLoading, isError } = useGrid(type);
@@ -74,7 +46,7 @@ const Grid: React.FC<GridProps> = ({ type }) => {
                   return (
                     <div
                       key={i}
-                      className={`cell ${i % 2 === 0 ? 'even' : 'odd'} ${classNames[key]} ${
+                      className={`cell ${classNames[key]} ${i % 2 === 0 ? 'even' : 'odd'} ${
                         key === 'ALARMSEVERITY' && value === '경고' ? 'warning' : 'critical'
                       }`}
                       title={key === 'INC' ? `${Math.abs(value as number)}%` : value.toLocaleString()}
---
+++ src/components/Grid/data.ts
@@ -0,0 +1,28 @@
+interface IndexClassNames {
+  [index: string]: string;
+}
+
+interface IndexColumns {
+  [index: string]: string[];
+}
+
+export const classNames: IndexClassNames = {
+  NO: 'no',
+  SERVICE_NAME: 'service-nm',
+  TEAM_NAME: 'team-nm',
+  CNT: 'cnt',
+  ALARMSEVERITY: 'grade',
+  ALARM_NAME: 'alarm-nm',
+  DAY: 'day',
+  THIS_WEEK: 'this-week',
+  LAST_WEEK: 'last-week',
+  INC: 'ratio',
+  CONDITIONLOGTEXT: 'log',
+};
+
+export const columns: IndexColumns = {
+  month: ['No', '서비스명', '팀명', '건수'],
+  notTaken: ['No', '팀명', '등급', '알람명', '기간', '비고'],
+  week: ['No', '알람명', '서비스명', '건수'],
+  prepare: ['No', '알람명', '서비스명', '금주', '전주', '증감율'],
+};
